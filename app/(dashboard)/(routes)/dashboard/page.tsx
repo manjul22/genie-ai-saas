3ae9e4110ce7c58e73421414ca21e601fb62d212
manjul22 <manjuljain93@gmail.com>
2023-10-03 22:54:03 +0530

    conversation open ai Api

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import { ArrowRight, MessageSquare, Music, ImageIcon, VideoIcon, Code } from "lucide-react";
